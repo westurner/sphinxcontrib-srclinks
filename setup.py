85a4854dcd99110d06cb6d9f23f3a216c0e1cd82
Wes Turner <wes@wrd.nu>
2014-12-31 18:24:19 -0600

    RLS: version 0.1.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requires = ['Sphinx>=0.6']
 
 setup(
     name='sphinxcontrib-srclinks',
-    version='0.1.1',
+    version='0.1.2',
     url='http://bitbucket.org/westurner/sphinxcontrib-srclinks',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-srclinks',
     license='BSD (3-clause)',
