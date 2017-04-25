2299bd61b415790ab9ac566a4f1dc680ed3f4242
Wes Turner <wes.turner@gmail.com>
2017-04-24 22:49:47 -0500

    BLD,DOC: setup.py: update url field

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ requires = ['Sphinx>=0.6']
 setup(
     name='sphinxcontrib-srclinks',
     version='0.2.1',
-    url='http://bitbucket.org/westurner/sphinxcontrib-srclinks',
+    url='https://github.com/westurner/sphinxcontrib-srclinks',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-srclinks',
     license='BSD (3-clause)',
     author='Wes Turner',
